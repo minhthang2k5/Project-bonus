fdfbae1f9ffe10deac4b461e2dbb0b4b101ea40a
trongtran7 <trongtran5002@gmail.com>
2024-05-09 22:46:59 +0700

    Tạo struct Student

--- ConsoleApplication1/student.h
+++ ConsoleApplication1/student.h
@@ -1,1 +1,36 @@
-#pragma once
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+#include <iostream>
+#include "teacher.h"
+#include "class.h"
+using namespace std;
+
+struct Date
+{
+    int day, month, year;
+};
+
+struct StudentFullName
+{
+    string firstName;
+    string lastName;
+};
+
+enum Gender
+{
+    male,
+    female
+};
+
+struct Student
+{
+    Class studentClass;
+    unsigned int studentID;
+    StudentFullName name;
+    Gender gender;
+    Date dateOfBirth;
+    unsigned int socialID;
+};
+
+#endif
