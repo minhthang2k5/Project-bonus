ed00b471e5b450833d8ebbe431f9403a86667ed1
minh thang <minhthang@gmail.com>
2024-06-06 13:58:47 +0700

    sửa hàm eatline

--- ConsoleApplication1/teacher.cpp
+++ ConsoleApplication1/teacher.cpp
@@ -1,11 +1,9 @@
-﻿#pragma once
-#include "teacher.h"
+﻿#include "teacher.h"
 #include "student.h"
 #include "class.h"
 void eatline()
 {
-	cin.clear();
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cin.ignore();
 }
 listYear creatListYear()
 {
